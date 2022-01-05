a41f42145fbd69e8f225469e57b5f92d806fda49
André <andremarks99@gmail.com>
2022-01-05 08:26:54 -0300

    Atualizacao

--- pokedex/src/HomePage/HomePage.js
+++ pokedex/src/HomePage/HomePage.js
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios'
 import { DivMain, DivHeader, DivPoke, DivCard, Img } from "./Styled";
+import { goToPokedex } from "../routes/coordinator"
+import { useNavigate } from 'react-router-dom'
 
 const HomePage = () => {
+
+  const navigate = useNavigate()
 
   const [pokemon, setPokemon] = useState([])
   const [newPokemon, setNewPokemon] = useState([])
 
-  useEffect(() => {
+  const getPokemons = () => {
     axios.get(`https://pokeapi.co/api/v2/pokemon?limit=20`)
     .then((res) => {
       setPokemon(res.data.results)
@@ -15,55 +19,63 @@ const HomePage = () => {
     .catch((err) => {
       console.log(err.response.data)
     })
+  }
+ 
+    const getNewPokemons = () => {
+      const newPoke = []
+
+      pokemon.forEach((poke) => {
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${poke.name}`)
+        .then((res) => {
+          newPoke.push(res.data)
+          if (newPoke.length === 20) {
+            const orderedList = newPoke.sort((a, b) => {
+              return a.id - b.id;
+            });
+            setNewPokemon(orderedList);
+          }
+        })
+        .catch((err) => {
+          console.log(err.response)
+        })
+      })
+      setNewPokemon(newPoke)
+      console.log(newPoke)
+    }
+  
+  useEffect(() => {
+    getPokemons()
   }, [])
 
   useEffect(() => {
-    const newPoke = []
-
-    pokemon.forEach((poke) => {
-      axios.get(`https://pokeapi.co/api/v2/pokemon/${poke.name}`)
-      .then((res) => {
-        newPoke.push(res.data)
-        if (newPoke.length === 20) {
-          const orderedList = newPoke.sort((a, b) => {
-            return a.id - b.id;
-          });
-          setNewPokemon(orderedList);
-        }
-      })
-      .catch((err) => {
-        console.log(err.response)
-      })
-    })
-    setNewPokemon(newPoke)
+    getNewPokemons()
   }, [pokemon])
 
-  console.log(newPokemon)
-  console.log(pokemon)
 
   const renderPokeList = newPokemon.map((poke, index) => {
-    return (
-      <DivCard key={index}>
-        <Img src={poke.sprites.front_default} alt={poke.name} />
-        <h3>{poke.name}</h3>
-        <button>Adicionar</button>
-        <button>Detalhes</button>
-      </DivCard>
-    )
-  })
+    console.log(newPokemon)
+      return (
+        <DivCard key={index}>
+          <Img src={poke.sprites.front_default} alt={poke.name} />
+          <h3>{poke.name}</h3>
+          <button>Adicionar</button>
+          <button>Detalhes</button>
+        </DivCard>
+      )
+    })
 
   return (
     <DivMain>
 
       <DivHeader>
-        <button>Ir para Pokedex</button>  
+        <button onClick={() => goToPokedex(navigate)}>Ir para Pokedex</button>  
         <div>
           <p>Lista de Pokémons</p>
         </div>
       </DivHeader>
       
       <DivPoke>
-        {renderPokeList}
+        {newPokemon && renderPokeList}
       </DivPoke>
  
     </DivMain>
