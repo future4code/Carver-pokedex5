405b473dcca2e57505354cb70b60b57b05236741
Luis Gustavo <luisgustavordp1@gmail.com>
2022-01-05 17:46:21 -0300

    primeiro commit - luis

--- pokedex/src/DetailsPage/DetailsPage.js
+++ pokedex/src/DetailsPage/DetailsPage.js
@@ -1,10 +1,72 @@
-import React from "react";
+import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { GlobalContext } from '../contexts/GlobalContext/GlobalStateContext';
+import { goToHome, goToPokedex } from '../routes/coordinator';
+import { CardDiv, HeaderDiv } from './Styled';
 
 function DetailsPage() {
-  
+  const navigate = useNavigate();
+  const [state, setState] = useState([]);
+  // const { pokemon, getPokemons } = useContext(GlobalContext);
+  const getPokeById = async (id) => {
+    if (id) {
+      try {
+        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+        console.log(res.data);
+        setState(res.data);
+      } catch (err) {
+        console.log(err.response.data);
+      }
+    }
+  };
+
+  // useEffect(() => {
+  //   getPokemons();
+  // }, []);
+
+  useEffect(() => {
+    getPokeById();
+  });
+  // const detailsPokemon = pokemon.map((poke) => {
+  //   return <h1>{poke.name}</h1>;
+  // });
+
+  console.log(state);
+
+  const detailsPokemon = state.map((item) => {
+    return (
+      <div>
+        <h1>{item && item.name}</h1>
+        <h1>{item && item.id}</h1>
+      </div>
+    );
+  });
+
   return (
-    <div>Details</div>
-  )
+    <>
+      <HeaderDiv>
+        <button
+          onClick={() => {
+            goToHome(navigate);
+          }}
+        >
+          Pokemon
+        </button>
+        <button
+          onClick={() => {
+            goToPokedex(navigate);
+          }}
+        >
+          Pokedex
+        </button>
+      </HeaderDiv>
+      <CardDiv>
+        {detailsPokemon}
+        Details
+      </CardDiv>
+    </>
+  );
 }
 
 export default DetailsPage;
---
+++ pokedex/src/DetailsPage/Styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const CardDiv = styled.div`
+  border: 3px solid black;
+  display: grid;
+  justify-items: center;
+`;
+
+export const HeaderDiv = styled.div`
+  border: 1px solid gray;
+  margin-bottom: 150px;
+  display: flex;
+  justify-content: center;
+`;
