931b5d91059e486ff2350e1253609aefbb0a26b5
Luis Gustavo <luisgustavordp1@gmail.com>
2022-01-06 11:00:36 -0300

    segundo commit - luis

--- pokedex/src/DetailsPage/DetailsPage.js
+++ pokedex/src/DetailsPage/DetailsPage.js
@@ -1,47 +1,64 @@
 import axios from 'axios';
 import React, { useContext, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { GlobalContext } from '../contexts/GlobalContext/GlobalStateContext';
 import { goToHome, goToPokedex } from '../routes/coordinator';
 import { CardDiv, HeaderDiv } from './Styled';
 
 function DetailsPage() {
   const navigate = useNavigate();
-  const [state, setState] = useState([]);
-  // const { pokemon, getPokemons } = useContext(GlobalContext);
-  const getPokeById = async (id) => {
-    if (id) {
-      try {
-        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`);
-        console.log(res.data);
-        setState(res.data);
-      } catch (err) {
-        console.log(err.response.data);
-      }
-    }
-  };
+  const { name } = useParams();
+  const [details, setDetails] = useState([]);
+  const { pokemon, getPokemons } = useContext(GlobalContext);
+  // const getPokeById = async (id) => {
+  //   if (id) {
+  //     try {
+  //       const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+  //       console.log(res.data);
+  //       setState(res.data);
+  //     } catch (err) {
+  //       console.log(err.response.data);
+  //     }
+  //   }
+  // };
 
   // useEffect(() => {
   //   getPokemons();
   // }, []);
 
-  useEffect(() => {
-    getPokeById();
-  });
+  // useEffect(() => {
+  //   getPokeById();
+  // });
   // const detailsPokemon = pokemon.map((poke) => {
   //   return <h1>{poke.name}</h1>;
   // });
 
-  console.log(state);
-
-  const detailsPokemon = state.map((item) => {
-    return (
-      <div>
-        <h1>{item && item.name}</h1>
-        <h1>{item && item.id}</h1>
-      </div>
-    );
-  });
+  useEffect(() => {
+    let pokeDetails = [];
+    if (pokeDetails === []) {
+      pokeDetails = getPokemons.find((item) => {
+        return item.name === name;
+      });
+    } else {
+      pokeDetails = pokemon.find((item) => {
+        return item.name === name;
+      });
+    }
+    if (!pokeDetails) {
+      axios
+        .get(`https://pokeapi.co/api/v2/pokemon/${name}/`)
+        .then((res) => {
+          setDetails(res.data);
+          console.log(pokeDetails);
+          console.log(details);
+        })
+        .catch((err) => {
+          console.log(err.response);
+        });
+    } else {
+      setDetails(pokeDetails);
+    }
+  }, []);
 
   return (
     <>
@@ -62,7 +79,15 @@ function DetailsPage() {
         </button>
       </HeaderDiv>
       <CardDiv>
-        {detailsPokemon}
+        {details &&
+          details.map((item) => {
+            return (
+              <div>
+                <h1>{item && item.name}</h1>
+                <h1>{item && item.id}</h1>
+              </div>
+            );
+          })}
         Details
       </CardDiv>
     </>
--- pokedex/src/routes/Router.js
+++ pokedex/src/routes/Router.js
@@ -1,20 +1,19 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import HomePage from '../HomePage/HomePage'
-import PokedexPage from '../PokedexPage/PokedexPage'
-import DetailsPage from '../DetailsPage/DetailsPage'
-
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from '../HomePage/HomePage';
+import PokedexPage from '../PokedexPage/PokedexPage';
+import DetailsPage from '../DetailsPage/DetailsPage';
 
 const Router = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<HomePage />}/>
-                <Route path="/pokedex" element={<PokedexPage />}/>
-                <Route path="/detalhes" element={<DetailsPage />}/>
-            </Routes>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/pokedex" element={<PokedexPage />} />
+        <Route path="/detalhes/:name/" element={<DetailsPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default Router
+export default Router;
